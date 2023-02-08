9e093f86eeae43f3b64aea93129d1f900ce09011
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-02-08 19:28:58 +0100

    Add active class removal for articles on link click

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // document.getElementById('test-button').addEventListener('click', function(){
 //     const links = document.querySelectorAll('.titles a');
 //     console.log('links:', links);
@@ -8,10 +10,20 @@ const titleClickHandler = function(event){
     console.log(event);
 
     /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
 
     /* add class 'active' to the clicked link */
 
     /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.posts article.active');
+
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
 
     /* get 'href' attribute from the clicked link */
 
