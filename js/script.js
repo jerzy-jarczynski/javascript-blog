03b998a6831fe80c0aa8aae59248730271bab89c
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-02-08 19:57:48 +0100

    Add class active to article after click

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
 // });
 
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
 
@@ -27,11 +28,14 @@ const titleClickHandler = function(event){
         activeArticle.classList.remove('active');
     }
 
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
 
-    /* find the correct article using the selector (value of 'href' attribute) */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
 
-    /* add class 'active' to the correct article */
+    /* [DONE] add class 'active' to the correct article */
+    targetArticle.classList.add('active');
 }
   
 const links = document.querySelectorAll('.titles a');
