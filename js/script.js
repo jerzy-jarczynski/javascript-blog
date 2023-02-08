f8943195db6d7c5807be3d7de3cabfab25e653d6
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-02-08 19:38:36 +0100

    Add class active to link after click

--- js/script.js
+++ js/script.js
@@ -6,19 +6,21 @@
 // });
 
 const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event);
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement);
+    clickedElement.classList.add('active');
 
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
 
     for (let activeArticle of activeArticles) {
