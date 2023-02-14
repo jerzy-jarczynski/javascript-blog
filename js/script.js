bff933eb822d6c511f044ea495a990da298fc469
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-02-14 19:54:34 +0100

    Change project task-runner: add eslint, editorconfig, stylelint

--- js/script.js
+++ js/script.js
@@ -1,87 +1,85 @@
 {
-    
-    'use strict';
+  'use strict';
 
-    const titleClickHandler = function(event){
-        event.preventDefault();
-        const clickedElement = this;
+  const titleClickHandler = function(event) {
+    event.preventDefault();
+    const clickedElement = this;
 
-        /* [DONE] remove class 'active' from all article links  */
-        const activeLinks = document.querySelectorAll('.titles a.active');
+    /* [DONE] remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-        for(let activeLink of activeLinks){
-            activeLink.classList.remove('active');
-        }
-
-        /* [DONE] add class 'active' to the clicked link */
-        clickedElement.classList.add('active');
-
-        /* [DONE] remove class 'active' from all articles */
-        const activeArticles = document.querySelectorAll('.posts article.active');
-
-        for (let activeArticle of activeArticles) {
-            activeArticle.classList.remove('active');
-        }
-
-        /* [DONE] get 'href' attribute from the clicked link */
-        const articleSelector = clickedElement.getAttribute('href');
-
-        /* [DONE] find the correct article using the selector (value of 'href' attribute) */
-        const targetArticle = document.querySelector(articleSelector);
-
-        /* [DONE] add class 'active' to the correct article */
-        targetArticle.classList.add('active');
-
-        /* [DONE] Get and set height for every article */
-        const container = document.querySelector('.posts');
-        const activeHeight = document.querySelector('.posts article.active').offsetHeight;
-        container.style.height = activeHeight + 'px';
+    for(let activeLink of activeLinks) {
+      activeLink.classList.remove('active');
     }
 
-    const optArticleSelector = '.post',
-          optTitleSelector = '.post-title',
-          optTitleListSelector = '.titles';
+    /* [DONE] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
 
-    function generateTitleLinks() {
+    /* [DONE] remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.posts article.active');
 
-        /* [DONE] remove contents of titleList */
-        const titleList = document.querySelector(optTitleListSelector);
-        titleList.innerHTML = '';
-
-        /* [DONE] find all the articles and save them to variable: articles */
-        const articles = document.querySelectorAll(optArticleSelector);
-
-        let html = '';
-
-        for (let article of articles) {
-            /* [DONE] get the article id */
-            const articleId = article.getAttribute('id');
-
-            /* [DONE] find the title element */
-            /* [DONE] get the title from the title element */
-            const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-
-            /* [DONE] create HTML of the link */
-            const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-
-            /* [DONE] insert link into html variable */
-            html = html + linkHTML;
-        }
-
-        titleList.innerHTML = html;
-
-        const links = document.querySelectorAll('.titles a');
-    
-        for(let link of links){
-            link.addEventListener('click', titleClickHandler);
-        }
-
+    for (let activeArticle of activeArticles) {
+      activeArticle.classList.remove('active');
     }
 
-    generateTitleLinks();
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
 
-    document.querySelector('.posts').style.height = document.querySelector('.posts article.active').offsetHeight + 'px';
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
 
-    document.getElementById('myVideo').play();
+    /* [DONE] add class 'active' to the correct article */
+    targetArticle.classList.add('active');
 
+    /* [DONE] Get and set height for every article */
+    const container = document.querySelector('.posts');
+    const activeHeight = document.querySelector('.posts article.active').offsetHeight;
+    container.style.height = activeHeight + 'px';
+  };
+
+  const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+  const generateTitleLinks = function() {
+
+    /* [DONE] remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+
+    /* [DONE] find all the articles and save them to variable: articles */
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    let html = '';
+
+    for (let article of articles) {
+      /* [DONE] get the article id */
+      const articleId = article.getAttribute('id');
+
+      /* [DONE] find the title element */
+      /* [DONE] get the title from the title element */
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+      /* [DONE] create HTML of the link */
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+
+      /* [DONE] insert link into html variable */
+      html = html + linkHTML;
+    }
+
+    titleList.innerHTML = html;
+
+    const links = document.querySelectorAll('.titles a');
+
+    for(let link of links){
+      link.addEventListener('click', titleClickHandler);
+    }
+
+  };
+
+  generateTitleLinks();
+
+  document.querySelector('.posts').style.height = document.querySelector('.posts article.active').offsetHeight + 'px';
+
+  document.getElementById('myVideo').play();
 }
